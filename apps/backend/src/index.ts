ef8e825709518103c232b934806800fd59f7b901
Senthil Athiban <senthilathiban0@gmail.com>
2024-12-12 14:26:54 +0530

    (fix) comments

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -7,11 +7,8 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-console.log("-----reached--------");
-// middleware
+
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/zap", zapRouter);
-app.use("/api/v2/zap", zapRouter); // dummy middleware
 
-// port
 app.listen(8080, () => console.log('listening on', 8080));
