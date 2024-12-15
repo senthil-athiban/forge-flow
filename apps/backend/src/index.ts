d339b6bdb3756dfaa93400dbe0763c9bf82eb3ad
Senthil Athiban <senthilathiban0@gmail.com>
2024-12-15 19:42:28 +0530

    (fix) import domain from env

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -4,7 +4,9 @@ import { userRouter } from "./routes/user";
 import { zapRouter } from "./routes/zap.router.";
 import { triggerRouter } from "./routes/trigger.router";
 import { actionRouter } from "./routes/action.router";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
 
 app.use(express.json());
@@ -16,4 +18,4 @@ app.use("/api/v1/trigger", triggerRouter);
 app.use("/api/v1/action", actionRouter);
 
 
-app.listen(8080, () => console.log('listening on', 8080));
+app.listen(process.env.PORT, () => console.log('listening on', process.env.PORT));
