df1bd222ad590fe1c7600a2cd782618eca2b6528
Senthil Athiban <senthilathiban0@gmail.com>
2024-12-17 13:34:31 +0530

    (feat) added_metadata_col_in_zaprun

--- apps/hooks/src/index.ts
+++ apps/hooks/src/index.ts
@@ -1,25 +1,28 @@
 import express from "express";
 import dotenv from 'dotenv';
 import { PrismaClient } from '@prisma/client';
+dotenv.config();
 
 const client = new PrismaClient();
-
-dotenv.config();
-const app = express();
 const PORT = process.env.DOMAIN;
+
+const app = express();
+app.use(express.json());
+
 
 //@ts-ignore
 app.post("/hooks/catch/:userId/:zapId", async (req, res) => {
     const userId = req.params.userId;
     const zapId = req.params.zapId;
-
+    const body = req.body;
     // check user exists
     // verify the zapId is belongs to the user
     console.log("zap id: ", zapId);
     const response = await client.$transaction(async (tx) => {
         const zapRun = await tx.zapRun.create({
             data: {
-                zapId: zapId
+                zapId: zapId,
+                metadata: body
             }
         });
 
