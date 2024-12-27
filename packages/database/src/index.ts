5d28774bc2fb3ef86f6a97e6747e03f792c14d6c
Senthil Athiban <senthilathiban0@gmail.com>
2024-12-27 13:41:30 +0530

    (fix) export types

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,1 +1,12 @@
 export * from '@prisma/client';
+
+export type {
+    User,
+    Zap,
+    TokenType,
+    Action,
+    Trigger,
+    ZapRun,
+    ActionTypes,
+    TriggerTypes
+  } from '@prisma/client';
