540f0739f178083ff9165fa52d3dbf245a8a4a54
Senthil Athiban <senthilathiban0@gmail.com>
2025-02-03 13:05:05 +0530

    (fix) kafka_message_producer

--- apps/sweeper/src/index.ts
+++ apps/sweeper/src/index.ts
@@ -1,18 +1,15 @@
-import { Kafka } from "kafkajs";
-import { prisma } from "@repo/db";
 import dotenv from "dotenv";
+import { prisma } from "@repo/db";
 import { KafkaService } from "@repo/kafka";
 
 dotenv.config();
 
-// const kafka = new Kafka({
-//     clientId: process.env.CLIENT_ID || 'zap-app',
-//     brokers: [`${process.env.KAFKA_HOST || 'kafka'}:${process.env.KAFKA_PORT || '9092'}`]
-// });
-
 const kafka = KafkaService.getInstance();
+
 const main = async () => {
+
   await kafka.start();
+
   await kafka.createTopics([
     {
       topic: process.env.TOPIC_NAME!,
@@ -27,22 +24,21 @@ const main = async () => {
       take: 10,
     });
 
-    if (data?.length > 0) console.log(" data : ", data);
-    await kafka.produceMessage({
-      topic: process.env.TOPIC_NAME!,
-      message: data?.map((item) => ({
-        value: JSON.stringify({ zapRunId: item.zapRunId, stage: 0 }),
-      })),
-    });
-
-    await prisma.zapRunOutBox.deleteMany({
-      where: {
-        id: {
-          in: data?.map((item) => item.id),
+    
+    if (data?.length > 0) {
+      console.log(" data : ", data);
+      await kafka.produceMessage({
+        topic: process.env.TOPIC_NAME!,
+        message: data?.map((item) => ({value: {zapRunId: item.zapRunId, stage: 0}}))
+      });
+      await prisma.zapRunOutBox.deleteMany({
+        where: {
+          id: {
+            in: data?.map((item) => item.id),
+          },
         },
-      },
-    });
+      });
+    }
   }
 };
-
 main().catch(console.error);
