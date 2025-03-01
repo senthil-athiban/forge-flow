f590eb05f100fc10797b6bbb16cca5d29e0a67a5
Senthil Athiban <senthilathiban0@gmail.com>
2025-03-01 20:20:28 +0530

    (fix) revert_changes

--- apps/client/app/config.ts
+++ apps/client/app/config.ts
@@ -6,7 +6,5 @@ export const NO_SIDEBAR_ROUTES = [
   "/slack/success",
 ];
 
-console.log('process.env.NEXT_PUBLIC_BACKEND_DOMAIN:', process.env.NEXT_PUBLIC_BACKEND_DOMAIN);
-console.log('process.env.SLACK_CLIENT_ID:', process.env.NEXT_PUBLIC_SLACK_CLIENT_ID);
 export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_DOMAIN!;
 export const SLACK_CLIENT_ID = process.env.NEXT_PUBLIC_SLACK_CLIENT_ID!;
