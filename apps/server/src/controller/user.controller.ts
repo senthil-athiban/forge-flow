53e1cc5aeb8bda3f7071cf06d4a95f458060cc81
Senthil Athiban <senthilathiban0@gmail.com>
2025-03-04 23:41:23 +0530

    (feat) add_seo_meta_data

--- apps/server/src/controller/user.controller.ts
+++ apps/server/src/controller/user.controller.ts
@@ -3,7 +3,6 @@ import userService from "../services/user.service";
 import { Request, Response } from "express";
 
 const getUser = asyncMiddleWare(async (req: Request, res: Response) => {
-  console.log('incoming for getuser');
   const userId = req.userId as string;
   const user = await userService.verifyUser(userId);
   res.status(200).send({ user });
