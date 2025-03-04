48c363ca88e32353682403b46e60b8ef8ee080c4
Senthil Athiban <senthilathiban0@gmail.com>
2025-03-05 01:41:24 +0530

    (fix) next_config

--- apps/client/next.config.js
+++ apps/client/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'standalone',
+  output: 'export', // Enable static export
+  trailingSlash: true, // Recommended for static exports
+  images: {
+    unoptimized: true // Important for static export
   }
-  
-  module.exports = nextConfig
+}
+
+module.exports = nextConfig
