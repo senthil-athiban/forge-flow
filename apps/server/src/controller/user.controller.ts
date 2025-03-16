49e2a8c56d0bb4ca87be1cf63ba9f7bd663d7255
Senthil Athiban <senthilathiban0@gmail.com>
2025-03-16 23:25:58 +0530

    (feat) add_edit_profile_route

--- apps/server/src/controller/user.controller.ts
+++ apps/server/src/controller/user.controller.ts
@@ -1,3 +1,4 @@
+import { ApiError } from "@/config/error";
 import { asyncMiddleWare } from "../config/asyncMiddleware";
 import userService from "../services/user.service";
 import { Request, Response } from "express";
@@ -8,4 +9,13 @@ const getUser = asyncMiddleWare(async (req: Request, res: Response) => {
   res.status(200).send({ user });
 });
 
-export default { getUser };
+const editUser = asyncMiddleWare( async (req: Request, res: Response) => {
+  const userId = req.userId as string;
+  const user = await userService.verifyUser(userId);
+  if(!user) {
+    throw new ApiError(401, "Un authorized to edit");
+  }
+  //TODO: check from client api
+})
+
+export default { getUser, editUser };
--- apps/server/src/routes/user.router.ts
+++ apps/server/src/routes/user.router.ts
@@ -5,5 +5,5 @@ import userController from "../controller/user.controller";
 const router = Router();
 
 router.get("/me", authMiddleware, userController.getUser);
-
+router.post("/profile/:userId", authMiddleware, userController.editUser)
 export const userRouter = router;
